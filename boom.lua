e7986ffe82d1ccdc90142164e4f55ddf0ae99221
TheMaverickProgrammer <91709+TheMaverickProgrammer@users.noreply.github.com>
2024-04-06 20:26:21 -0400

    updated lib to conform to the new output of boomsheets. Updated tests to be more rigorous. Changed API to reflect existing game engine expectations. Added plenty of contents. NOTE THAT THIS LIB IS NOT FULLY COMPLIANT WITH THE FILE FORMAT SPEC BOOMSHEETS USES TO PARSE!

--- boom.lua
+++ boom.lua
@@ -3,34 +3,47 @@
     @github https://github.com/TheMaverickProgrammer/boomsheetslua
     @module boom
     @date 1/23/2021
-    @description This is a lua port of the boomsheet animation file format for 2D spritesheets
+    @updated 4/6/2024
+    @description This is PARTIAL lua port of the boomsheet anim file format for 2D spritesheets
+    @warning It can be used as-is withs files from boomsheets but does not comply to the full file format spec!
 
     @example
     ```
-    anim = boom.load(path)        -- loads the animation from path and stores it in the anim table
-    t    = anim:duration(state)   -- returns the duration of the animation state in seconds
-    f    = anim:refresh(state)    --  sets `elapsed` to zero and applies the first frame from that state
-    f    = anim:update(state,sec) -- updates animation by seconds elapsed and returns the frame
+    anim = boom.load(path)       -- loads the animation from path and stores it in the anim table
+    t    = anim:duration(state)  -- returns the duration of the animation state in frames
+    f    = anim:set(state)       -- sets `elapsed` ticks to zero and applies the first frame from that state
+    f    = anim:update(ticks)    -- updates animation by ticks elapsed and returns the current frame
 
-    print(anim.imagePath) -- prints the image path
-    print(anim.elapsed)   -- the elapsed time for this animation
-    curr = anim.currFrame -- can also access the current frame this way
-    
-    anim.states.ATTACK_PUNCH.loop = true -- tells this animation state to loop (default false)
+    print(anim.image_path)       -- prints the image path
+    print(anim.elapsed)          -- the elapsed time for this animation in ticks (hertz agnostic)
+    anim.curr_frame              -- can access the a copy of the frame's table
+    anim.curr_state              -- can access the last set state table ref
+    anim.curr_state_name         -- string name of the last set state
+    anim.states.ATTACK_PUNCH.loop = true -- tell this animation state to loop (default false)
     ```
 --]]
 
 local boom = {}
 
--- Load a *.animation file at `path`. Return true if parsing was successful, false otherwise.
+-- Load a *.anim file at `path`. Return true if parsing was successful, false otherwise.
 function boom.load(path)
     -- animations keep a list of all states, elapsed time, image path, 
     -- and the lookup for the recent frame
-    local animation = {states={},elapsed=0,imagePath="",currFrame={}}
+    local animation = {
+        elapsed=0,
+        curr_state_name="",
+        image_path="",
+        states={},
+        curr_frame={},
+        curr_state={},
+}
 
     -- @return result of removed quotes in input string
-    function cleanstr(str)
-        str, _ = str:gsub('\"', '')
+    local function cleanstr(str)
+        if str ~= nil then
+            str, _ = str:gsub('\"', '')
+        end
+
         return str
     end
 
@@ -43,55 +56,59 @@ function boom.load(path)
         return time
     end
 
-    -- update the current frame and reset the timer over to zero
-    -- @return updated frame `currFrame`
-    function animation.refresh(self, state)
+    -- reset the current frame and the ticks to zero
+    -- @return changes `curr_frame` and `curr_state`
+    function animation.set(self, state)
         self.elapsed = 0
-        return animation.update(self, state, 0)
+        self.curr_state = self.states[state]
+        self.curr_state_name = state
+        return animation.update(self, 0)
     end
 
-    -- update the animation state by elapsed seconds
-    -- @return updated frame `currFrame`
-    function animation.update(self, state, seconds)
-        local currState = self.states[state]
-        self.elapsed = self.elapsed + seconds
+    -- update the animation state by elapsed ticks
+    -- @return updated frame `curr_frame`
+    function animation.update(self, ticks)
+        self.elapsed = self.elapsed + ticks
 
-        local duration = self:duration(state)
+        local duration = self:duration(self.curr_state_name)
 
         if self.elapsed > duration then
             self.elapsed = duration
         end
 
-        local e = self.elapsed 
+        local e = self.elapsed
         local index = 1
 
-        while e > 0 and index <= #currState.framelist do 
-            e = e - currState.framelist[index].duration
-            index = index + 1
+        while e > 0 and index <= #self.curr_state.framelist do
+            e = e - self.curr_state.framelist[index].duration
 
-            if index > #currState.framelist then
-                if currState.loop then
+            -- If true, this frame had time left to complete
+            if e > 0 then
+                index = index + 1
+            end
+
+            if index > #self.curr_state.framelist then
+                if self.curr_state.loop then
                     index = 1
-                    self.elapsed = self.elapsed - duration
                 else
                     index = index - 1
                 end
             end
         end
 
-        local currFrame = currState.framelist[index]
-        
-        -- copy values over to lookup table
-        for k,v in pairs(currFrame) do
-            self.currFrame[k] = v
+        local curr_frame = self.curr_state.framelist[index]
+
+        -- COPY values over to lookup table
+        for k,v in pairs(curr_frame) do
+            self.curr_frame[k] = v
         end
 
-        return self.currFrame
+        return self.curr_frame
     end
 
     -- extract the contents of the file
     local file = io.open(path, "rb")
-    if not file then 
+    if not file then
         -- failed to open? return nil
         return nil
     else
@@ -99,7 +116,7 @@ function boom.load(path)
         file:close()
     end
 
-    local currState = ""
+    local curr_state = ""
 
     -- for each line, tokenize everything between spaces
     for line in io.lines(path) do
@@ -109,31 +126,30 @@ function boom.load(path)
         end
 
         local key = tokens[1]
-        local imagePathKey = nil
-        
-        if key then 
-            imagePathKey = key:sub(0,9)
-        end
-        
+
         -- most keys have attributes in the form of "key=value"
-        -- some keys like "imagePath" are directly assigned to their value
-        if  imagePathKey == "imagePath" then
-            animation.imagePath = cleanstr(key:sub(11))
-        elseif key == "animation" then 
+        -- some keys like "image_path" are directly assigned to their value
+        if  key == "!image_path" then
+            animation.image_path = cleanstr(tokens[2])
+        elseif key == "!frame_rate" then
+            animation.frame_rate = tonumber(cleanstr(tokens[2]))
+        elseif key == "animation" or key == "anim" then
             -- add a new state table to our animation
-            local attrs = {}
+            local fields = {}
             for token in tokens[2]:gmatch("([^=]+)") do
-                table.insert(attrs, token)
+                table.insert(fields, token)
             end
 
-            if not attrs[2] then 
-                return nil -- malformed
+            local state = fields[2]
+
+            if not state then
+                state = fields[1] -- optional no "state" key being used
             end
 
-            currState = cleanstr(attrs[2])
+            curr_state = cleanstr(state)
 
             -- prepopulate our state table with framelist and set looping to false
-            animation.states[currState] = {framelist={},loop=false}
+            animation.states[curr_state] = {framelist={},loop=false}
         elseif key == "frame" then
             -- remove the key, we already accounted for it
             table.remove(tokens, 1)
@@ -147,11 +163,26 @@ function boom.load(path)
                 local is_key = true
                 local last_key = nil -- track the key
                 for token in v:gmatch("([^=]+)") do
+                    token = cleanstr(token)
                     if is_key then
-                        last_key = cleanstr(token)
-                    else
-                        -- extract token value between quotes
-                        local value = tonumber(cleanstr(token))
+                        last_key = token
+                        -- "duration" can be shortened to "dur"
+                        if last_key == "dur" then
+                            --[[ 
+                                Expand so that in lua the prop is always
+                                accessed by `.duration`
+                            --]]
+                            last_key = "duration"
+                        end
+                    elseif last_key then -- extract value
+
+                        -- Edge case: frames have an f at the end
+                        local tail = #token
+                        if token:sub(tail) == "f" then
+                            token = token:sub(0, tail-1)
+                        end
+
+                        local value = tonumber(token)
 
                         -- set the table
                         frame[last_key] = value
@@ -160,8 +191,8 @@ function boom.load(path)
                 end
             end
             -- add this new frame into our animation state's frame list
-            table.insert(animation.states[currState].framelist, frame)
-        end 
+            table.insert(animation.states[curr_state].framelist, frame)
+        end
     end
 
     -- return the fully constructed animation object
--- main.lua
+++ main.lua
@@ -1,24 +1,60 @@
 #!/usr/bin/lua5.1
 
-boom = require "boom"
+--[[
+    @author TheMaverickProgrammer
+    @github https://github.com/TheMaverickProgrammer/boomsheetslua
+    @date 1/23/2021
+    @description Test this minimal port of the boom lua lib
+--]]
 
-local anim = boom.load("data/test.animation")
+local boom = require "boom"
+local anim = boom.load("data/test.anim")
 
 if not anim then
     print("animation object was nil")
     os.exit()
 end
 
-print("anim sprite path is: "..anim.imagePath)
+print("===============\n=RUNNING TESTS=\n===============")
+
+print("anim sprite path is: "..anim.image_path)
 
 for k,v in pairs(anim.states) do
     print("state="..k)
 end
 
-print("Duration of SHOOT3 is "..anim:duration("SHOOT3"))
+-- Calculate frames (but do not switch state!)
+local duration = anim:duration("SHOOT3")
 
-anim:refresh("SHOOT3")
+-- If framerate is set, frame durations are in ticks per refresh rate (hz)
+if anim.frame_rate ~= nil then
+    duration = duration.." frames @ "..anim.frame_rate.." hz"
+else
+    -- Otherwise, support legacy seconds...
+    duration = duration.." seconds"
+end
 
-print("origin=(x="..anim.currFrame.originx..",y="..anim.currFrame.originy..")")
+print("Duration of SHOOT3 is "..duration)
 
-print("Done")
+-- Now try changing states
+anim:set("SHOOT3")
+print("origin=(x="..anim.curr_frame.originx..",y="..anim.curr_frame.originy..")")
+
+if anim.frame_rate ~= nil then
+    anim:update(18) -- add 18 ticks of time to animation
+else
+    anim:update(18*60) -- convert to legacy seconds
+end
+
+-- print the frame data
+print("current state="..anim.curr_state_name)
+print("elapsed ticks="..anim.elapsed)
+print("x="..anim.curr_frame.x..",y="..anim.curr_frame.y..",w="..anim.curr_frame.w..",h="..anim.curr_frame.h)
+
+ -- change state resets
+anim:set("IDLE3")
+print("current state="..anim.curr_state_name)
+print("elapsed ticks="..anim.elapsed)
+print("x="..anim.curr_frame.x..",y="..anim.curr_frame.y..",w="..anim.curr_frame.w..",h="..anim.curr_frame.h)
+
+print("==========\n=COMPLETE=\n==========")
